875253702703b968cee3bf5919280a2c423007fe
freezy <freezy@xbmc.org>
2013-03-02 20:21:09 +0100

    Added: Some more layout tweaks.

--- app/public/js/global.js
+++ app/public/js/global.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
 
 	// rotate blood stains
-	var total = 12;
+/*	var total = 12;
 	$('.modal-header').each(function() {
 		var randNum = Math.floor((Math.random()*total)+1);
 		var m = randNum > 9 ? randNum : '0' + randNum;
 		$(this).css('background-image', $(this).css('background-image').replace(/(\d)+/, m));
-	});
+	});*/
 
 });
