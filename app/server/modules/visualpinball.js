258b6693915b070296fb68c1e36bd7e8d5971dcd
freezy <freezy@xbmc.org>
2013-03-09 01:36:18 +0100

    Added: Basic ROM retrieval from .vpt file works.

--- app/server/modules/visualpinball.js
+++ app/server/modules/visualpinball.js
@@ -1,25 +1,60 @@
 var fs = require('fs');
+var log = require('winston');
 var ocd = require('ole-doc').OleCompoundDoc;
 var config =  require('konphyg')(__dirname + '../../../config');
 var settings = config('settings');
 
 /**
- * scripts start after 04 00 00 00 43 4F 44	45 (0x04 0 0 0 "CODE")
- * and ends before 04 00 00 00 00 as well.
+ * Returns the ROM name for a given table.
+ *
+ * Tries to determine which ROM is used for a table. It does by looking
+ * at the table script and intelligently guessing the lastly used ROM.
+ *
+ * @param tablePath Path to the .vpt file
+ * @param callback
  */
-
-
 exports.getGameRomName = function(tablePath, callback) {
 	exports.getScriptFromTable(tablePath, function(err, script) {
 		var m = script.match(/\.GameName\s+=\s+([^\s]+)/);
 		if (m) {
-			console.log('variable : ' + m[1]);
+
+			var getVariableValue = function(name) {
+				var regex = new RegExp(name + '\\s*=\\s*(.+)');
+				var m = script.match(regex);
+				return m ? m[1] : null;
+			}
+			var gameName = getVariableValue(m[1]);
+
+			// direct hit, all good.
+			if (gameName.indexOf('"') == 0) {
+				callback(null, gameName.match(/"([^"]+)/)[1]);
+				return;
+			}
+
+			console.log('value:' + gameName);
+
+			callback('Could not find currently used ROM in script.');
+
 		} else {
-			console.log('not found.');
+			callback('Could not find ".GameName" in the script anywhere.');
 		}
 	})
 }
 
+/**
+ * Returns a specific setting for a given table.
+ *
+ * Table settings are saved using SaveValue within a table script, which instructs
+ * VP to store the value somewhere. VP uses Microsoft's CFBF do store the data
+ * in a file called VPReg.stg.
+ *
+ * See also http://en.wikipedia.org/wiki/Compound_File_Binary_Format.
+ * Fortunately there's already a JS implementation that reads CFBF.
+ *
+ * @param storageKey The name of the storage
+ * @param streamKey The name of the "file" in the storage
+ * @param callback
+ */
 exports.getTableSetting = function(storageKey, streamKey, callback) {
 	var doc = new ocd(settings.visualpinball.path + '/User/VPReg.stg');
 	doc.on('err', function(err) {
@@ -29,19 +64,28 @@ exports.getTableSetting = function(storageKey, streamKey, callback) {
 		var stream = doc.storage(storageKey).stream(streamKey);
 		stream.on('data', function(buf) {
 			var data = buf.toString();
-			console.log('got buffer at %d bytes length: %s', buf.length, data);
+			log.debug('[vp] [ole] Got buffer at ' + buf.length + ' bytes length: ' + data);
 			callback(null, parseInt(data.replace(/\0+/g, '')));
 		});
 	});
 	doc.read();
 }
 
+/**
+ * Extracts the table script from a given .vpt file.
+ *
+ * Table scripts are at the end of the .vpt file. The header of the chunk equals
+ * 04 00 00 00 43 4F 44	45 (0x04 0 0 0 "CODE") and ends with 04 00 00 00.
+ *
+ * @param tablePath
+ * @param callback
+ */
 exports.getScriptFromTable = function(tablePath, callback) {
 	var now = new Date().getTime();
 	fs.open(tablePath, 'r', function(err, fd) {
 		var stat = fs.fstatSync(fd);
 		var buf = new Buffer(8)
-		console.log('Read table: %j', stat);
+		log.debug('[vp] [script] Found ' + tablePath + ' at ' + stat.size + ' bytes.');
 		var scriptStart, scriptEnd;
 		for (var i = stat.size; i > 0; i--) {
 			fs.readSync(fd, buf, 0, buf.length, i - buf.length);
@@ -55,7 +99,7 @@ exports.getScriptFromTable = function(tablePath, callback) {
 			}
 		}
 		var buf = new Buffer(scriptEnd - scriptStart);
-		console.log('Found positions %d and %d in %d ms.', scriptStart, scriptEnd, (new Date().getTime() - now));
+		log.debug('[vp] [script] Found positions ' + scriptStart + ' and ' + scriptEnd + ' in ' + (new Date().getTime() - now) + ' ms.');
 		fs.readSync(fd, buf, 0, buf.length, scriptStart);
 		callback(null, buf.toString());
 	});
