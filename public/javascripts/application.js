7f81217fcc5e2941525fec280a5de15817d4e044
freezy <freezy@xbmc.org>
2013-03-29 13:33:19 +0100

    Added: Correct navigation menu highlighting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 
 	// modal boxes
 	$('.modal-form-errors').modal({ show : false, keyboard : true, backdrop : true});
+
 });
+
+function menu(id) {
+	$('.navbar-inner ul.nav li').removeClass('active');
+	$('#' + id).parents('li').addClass('active');
+}
