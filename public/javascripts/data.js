0f260a54070267b8fb60ea9b67dc2515284003eb
freezy <freezy@xbmc.org>
2013-04-08 00:36:15 +0200

    Added: Basic user table now working.

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,6 +1,6 @@
 function enableData(config) {
 	
-	if (!config.dataId) {
+	if (!config.id) {
 		throw new Error('Must provide data ID when enabling data.');
 	}
 	if (!config.renderRows) {
@@ -12,12 +12,12 @@ function enableData(config) {
 
 	// enable items/page dropdown
 	$('.data.' + config.renderResult + ' select.numrows').change(function() {
-		$('.data.' + config.dataId + ' .pagination ul').data('page', 1);
+		$('.data.' + config.id + ' .pagination ul').data('page', 1);
 		refreshData(config);
 	});
 
 	// enable prev/next buttons
-	$('.data.' + config.dataId + ' .pagination li.first a').click(function(event) {
+	$('.data.' + config.id + ' .pagination li.first a').click(function(event) {
 		event.preventDefault();
 		var $ul = $(this).parents('ul');
 		var page = parseInt($ul.data('page'));
@@ -26,7 +26,7 @@ function enableData(config) {
 			refreshData(config);
 		}
 	});
-	$('.data.' + config.dataId + ' .pagination li.last a').click(function(event) {
+	$('.data.' + config.id + ' .pagination li.last a').click(function(event) {
 		event.preventDefault();
 		var $ul = $(this).parents('ul');
 		var page = parseInt($ul.data('page'));
@@ -38,7 +38,7 @@ function enableData(config) {
 	});
 
 	// enable filters
-	$('.data.' + config.dataId + ' ul.filter li a').click(function(event) {
+	$('.data.' + config.id + ' ul.filter li a').click(function(event) {
 		event.preventDefault();
 		$(this).parents('li').toggleClass('active');
 		$('.pagination ul').data('page', 1);
@@ -47,19 +47,19 @@ function enableData(config) {
 
 	// enable clear filters button
 	var clearFilters = function() {
-		$('.data.' + config.dataId + ' ul.filter li.active').removeClass('active');
-		$('.data.' + config.dataId + ' .pagination ul').data('page', 1);
+		$('.data.' + config.id + ' ul.filter li.active').removeClass('active');
+		$('.data.' + config.id + ' .pagination ul').data('page', 1);
 		refreshData(config);
 	}
-	$('.data.' + config.dataId + ' .noresult button').click(clearFilters);
+	$('.data.' + config.id + ' .noresult button').click(clearFilters);
 }
 
 function refreshData(config) {
-	var limit = $('.data.' + config.dataId + ' select.numrows').val();
-	var offset = ($('.data.' + config.dataId + ' .pagination ul').data('page') - 1) * limit;
+	var limit = $('.data.' + config.id + ' select.numrows').val();
+	var offset = ($('.data.' + config.id + ' .pagination ul').data('page') - 1) * limit;
 	var filters = [];
 
-	$('.data.' + config.dataId + ' ul.filter li.active').each(function() {
+	$('.data.' + config.id + ' ul.filter li.active').each(function() {
 		filters.push($(this).data('filter'));
 	});
 
@@ -76,13 +76,13 @@ function refreshData(config) {
 function updateData(config, response) {
 
 	// retrieve variables
-	var numrows = $('.data.' + config.dataId + ' select.numrows').val();
+	var numrows = $('.data.' + config.id + ' select.numrows').val();
 	var pages = Math.ceil(response.count / numrows);
-	var page = $('.data.' + config.dataId + ' .pagination ul').data('page');
-	var resultsFiltered = $('.data.' + config.dataId + ' ul.filter li.active').length > 0;
+	var page = $('.data.' + config.id + ' .pagination ul').data('page');
+	var resultsFiltered = $('.data.' + config.id + ' ul.filter li.active').length > 0;
 
 	// update pagination
-	$('.data.' + config.dataId + ' .pagination li:not(.first):not(.last)').remove();
+	$('.data.' + config.id + ' .pagination li:not(.first):not(.last)').remove();
 	for (var i = pages; i > 0; i--) {
 		var li = $('<li class="p' + i + (page == i ? ' current' : '') + '"><a href="#">' + i + '</a>');
 		if (page != i) {
@@ -96,42 +96,42 @@ function updateData(config, response) {
 				event.preventDefault();
 			});
 		}
-		$('.data.' + config.dataId + ' .pagination li.first').after(li);
+		$('.data.' + config.id + ' .pagination li.first').after(li);
 	}
-	$('.data.' + config.dataId + ' .pagination li').removeClass('disabled');
+	$('.data.' + config.id + ' .pagination li').removeClass('disabled');
 	if (page == 1) {
-		$('.data.' + config.dataId + ' .pagination li.first').addClass('disabled');
+		$('.data.' + config.id + ' .pagination li.first').addClass('disabled');
 	}
 	if (page == pages || pages == 0) {
-		$('.data.' + config.dataId + ' .pagination li.last').addClass('disabled');
+		$('.data.' + config.id + ' .pagination li.last').addClass('disabled');
 	}
 
 	// results returned
 	if (response.rows && response.rows.length > 0) {
 
 		// run callback
-		config.renderRows($('.data.' + config.dataId + ' table tbody'), response.rows);
+		config.renderRows($('.data.' + config.id + ' table tbody'), response.rows);
 
-		$('.data.' + config.dataId).show();
-		$('.data.' + config.dataId + ' .wrapper').slideDown(500);
-		$('.data.' + config.dataId + ' + div.empty').fadeOut(200);
-		$('.data.' + config.dataId + ' .noresult').fadeOut(200);
+		$('.data.' + config.id).show();
+		$('.data.' + config.id + ' .wrapper').slideDown(500);
+		$('.data.' + config.id + ' + div.empty').fadeOut(200);
+		$('.data.' + config.id + ' .noresult').fadeOut(200);
 
 	// no results due to filtering
 	} else if (resultsFiltered) {
 
-		$('.data.' + config.dataId).show();
-		$('.data.' + config.dataId + ' .wrapper').slideUp(200);
-		$('.data.' + config.dataId + ' + div.empty').hide();
-		$('.data.' + config.dataId + ' .noresult').fadeIn(500);
+		$('.data.' + config.id).show();
+		$('.data.' + config.id + ' .wrapper').slideUp(200);
+		$('.data.' + config.id + ' + div.empty').hide();
+		$('.data.' + config.id + ' .noresult').fadeIn(500);
 
 	// no results due to no data
 	} else {
 
-		$('.data.' + config.dataId).hide();
-		$('.data.' + config.dataId + ' .wrapper').show();
-		$('.data.' + config.dataId + ' + div.empty').fadeIn(500);
-		$('.data.' + config.dataId + ' .noresult').hide();
+		$('.data.' + config.id).hide();
+		$('.data.' + config.id + ' .wrapper').show();
+		$('.data.' + config.id + ' + div.empty').fadeIn(500);
+		$('.data.' + config.id + ' .noresult').hide();
 	}
 }
 
