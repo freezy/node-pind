be9237abc09bf9ad6541140f8dc63183e124aaf6
freezy <freezy@xbmc.org>
2013-04-08 08:34:22 +0200

    Added: Missing JSs.

---
+++ public/javascripts/admin_users.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	var render = function($tbody, rows) {
+
+		$tbody.empty();
+		for (var i = 0; i < rows.length; i++) {
+
+			var tr = '<tr data-id=' + rows[i].key + '><td>' + rows[i].user + '</td>';
+			tr != '<td>' + rows[i].credits + '</td>';
+			tr += '</td></tr>';
+			$(tr).appendTo($tbody);
+		}
+	};
+
+	var config = {
+		id: 'users',
+		renderRows: render,
+		apiCall: 'User.GetAll'
+	};
+
+	// load data on startup
+	enableData(config);
+	refreshData(config);
+});
