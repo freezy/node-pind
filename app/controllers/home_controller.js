f4ef69fbf24ec1ad64ef890b473bc29428b1ff09
freezy <freezy@xbmc.org>
2013-04-14 20:43:38 +0200

    Added: Global high score list to main page.

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -1,9 +1,22 @@
+var util = require('util');
+var schema = require('./app/model/schema');
+
 load('application');
 before(use('requireUser'));
 
 action('index', function (context) {
-	this.title = 'Welcome!';
-	render();
+	var that = this;
+	that.title = 'Welcome!';
+	schema.sequelize.query(
+		'SELECT u.id, u.user, sum(h.points) as totalPoints ' +
+		'FROM users u, hiscores h ' +
+		'WHERE u.id = h.userId ' +
+		'GROUP BY u.id, u.user ' +
+		'ORDER BY totalPoints DESC'
+	).success(function(rows) {
+			that.hiscores = rows;
+		render();
+	});
 });
 
 action('coin', function (context) {
