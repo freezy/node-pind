94324460930bd79ca1c077f3104ef012cd321f52
freezy <freezy@xbmc.org>
2013-05-05 00:50:45 +0200

    Added: More responsive CSS to VPF table listing.

--- public/javascripts/admin_sources.js
+++ public/javascripts/admin_sources.js
@@ -9,17 +9,17 @@ $(document).ready(function() {
 
 			$parent.append($('<li class="span6 vpf item" data-id="' + row.fileId + '"><div class="thumbnail">' +
 				'<div class="pull-left thumb-wrapper">' +
-					'<div class="thumb"></div>' +
-//					'<div class="thumb" style="background-image: url(\'' + imgUrl + '\')" data-src="' + imgUrl + '"></div>' +
+//					'<div class="thumb"></div>' +
+					'<div class="thumb" style="background-image: url(\'' + imgUrl + '\')" data-src="' + imgUrl + '"></div>' +
 					'<div class="thumb-placeholder"></div>' +
 				'</div>' +
 				'<h3>' + row.title_trimmed + '</h3>' +
-				'<p class="pull-right updated">' + row.lastUpdateRel + '</p>' +
-				'<h4>' + row.info + '</h4>' +
+				'<p class="pull-right updated hidden-phone-small">' + row.lastUpdateRel + '</p>' +
+				'<h4 class="hidden-phone-small">' + row.info + '</h4>' +
 				'<ul class="pills small">' +
-					'<li><i class="icon user"></i><div class="author">' + row.author + '</div></li>' +
-					'<li><i class="icon circle-arrow-down" title="' + groupdigit(row.downloads) + '"></i><span class="hidden-desktop-small">' + groupdigit(row.downloads) + '</span></li>' +
-					'<li><i class="icon eye" title="' + groupdigit(row.views) + '"></i><span class="hidden-desktop-small">' + groupdigit(row.views) + '</span></li>' +
+					'<li><i class="icon user"></i><div class="author hidden-phone-small">' + row.author + '</div></li>' +
+					'<li><i class="icon circle-arrow-down" title="' + groupdigit(row.downloads) + '"></i><span class="hidden-desktop-small hidden-phone-small">' + groupdigit(row.downloads) + '</span></li>' +
+					'<li><i class="icon eye" title="' + groupdigit(row.views) + '"></i><span class="hidden-desktop-small hidden-phone-small">' + groupdigit(row.views) + '</span></li>' +
 					'<li class="link"><a href="http://www.vpforums.org/index.php?app=downloads&showfile=' + row.fileId + '" target="_blank"><i class="icon link"></i></a></li>' +
 					'<li class="link"><a href="#"><i class="icon download-alt"></i></a></li>' +
 				'</ul>' +
