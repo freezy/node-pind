e8f9636eda5bf9680d43af23df334da3ca7743de
freezy <freezy@xbmc.org>
2013-05-06 23:55:41 +0200

    Added: Switched to AngularJS for downloads page.

--- public/javascripts/admin_transfers.js
+++ public/javascripts/admin_transfers.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	var render = function($tbody, rows) {
+/*	var render = function($tbody, rows) {
 		$tbody.empty();
 		for (var i = 0; i < rows.length; i++) {
 			var row = rows[i];
@@ -19,6 +19,27 @@ $(document).ready(function() {
 
 	// load data on startup
 	enableData(config);
-	refreshData(config);
+	refreshData(config);*/
 });
 
+function TransferCtrl($scope, $http) {
+
+	$scope.transfers = [];
+	$scope.numrows = 10;
+
+	console.log('numrows = %s', $scope.numrows);
+
+	ngApi($http, 'Transfer.GetAll', {
+		limit: $scope.numrows
+//		filters: [ ],
+//		fields: [ ]
+	}, function(err, result) {
+		$scope.transfers = result.rows;
+	});
+}
+
+angular.module('phonecatServices', ['ngResource']).factory('Phone', function($resource) {
+	return $resource('phones/:phoneId.json', {}, {
+			query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+	});
+});
--- public/javascripts/tables.js
+++ public/javascripts/tables.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
 });
 
-
 function TablesCtrl($scope, $http) {
 
 	$scope.tables = [];
