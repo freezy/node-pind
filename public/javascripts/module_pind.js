c0ec9f4532666cdfd2eaaa12e4b2a047906aea8f
freezy <freezy@xbmc.org>
2013-05-09 16:39:22 +0200

    Migrated sources view to Angular.

--- public/javascripts/module_pind.js
+++ public/javascripts/module_pind.js
@@ -1,3 +1,54 @@
+/**
+ * Controller for JSON-RPC enabled lists.
+ * @param $scope
+ * @param Jsonrpc
+ * @constructor
+ */
+function DataCtrl($scope, Jsonrpc) {
+
+	$scope.data = [];
+
+	$scope.page = 1;
+	$scope.numpages = 1;
+	$scope.limit = 10;
+	$scope.resource = null;
+	$scope.search = '';
+	$scope.filters = [];
+
+	var refresh = function() {
+
+		if (!$scope.resource) {
+			return alert('Must set "resource" attribute somewhere in scope.');
+		}
+
+		var params = {
+			offset: ($scope.page - 1) * $scope.limit,
+			limit: $scope.limit
+		};
+
+		if ($scope.search && $scope.search.length != 1) {
+			params.search = $scope.search;
+		}
+
+		if ($scope.filters && $scope.filters.length > 0) {
+			params.filters = $scope.filters;
+		}
+
+		Jsonrpc.call($scope.resource, params, function(err, result) {
+			if (err) {
+				return alert(err);
+			}
+			$scope.data = result.rows;
+			$scope.numpages = Math.ceil(result.count / $scope.limit);
+			$scope.$broadcast('dataUpdated');
+		});
+	};
+
+	// refresh on explicit params updated event and as soon as resource is set.
+	$scope.$watch('resource', refresh);
+	$scope.$on('paramsUpdated', refresh);
+}
+
 
 var pindAppModule = angular.module('pind', []);
 
