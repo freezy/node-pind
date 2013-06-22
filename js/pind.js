c5983b1f9aa5cd81227accf163d25dc11580ac7a
freezy <freezy@xbmc.org>
2013-06-22 12:55:34 +0200

    Fixed: Small layouts.

--- js/pind.js
+++ js/pind.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function() {
         $(this).blur();
         dataslide = $(this).parents('li').attr('data-slide');
         htmlbody.animate({
-            scrollTop: $('.slide[data-slide="' + dataslide + '"]').prev('hr').offset().top - 40
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').prev('hr').offset().top - 38
         }, 1000, 'easeInOutQuint');
 
     });
