9e75a023fab625e2a16c37407ec383e7b5ff00bb
freezy <freezy@xbmc.org>
2013-09-09 23:20:20 +0200

    Fixed wrong table name (argh).

--- migrations/20130907131940-820a64c-add-ipdb-reference-to-vpf-file.js
+++ migrations/20130907131940-820a64c-add-ipdb-reference-to-vpf-file.js
@@ -3,10 +3,10 @@ var async = require('async');
 module.exports = {
 
 	up: function(migration, DataTypes, done) {
-		migration.addColumn('vpf_file', 'ipdb_id', DataTypes.INTEGER).complete(done);
+		migration.addColumn('vpf_files', 'ipdb_id', DataTypes.INTEGER).complete(done);
 	},
 
 	down: function(migration, DataTypes, done) {
-		migration.removeColumn('vpf_file', 'ipdb_id', DataTypes.INTEGER).complete(done);
+		migration.removeColumn('vpf_files', 'ipdb_id', DataTypes.INTEGER).complete(done);
 	}
 };
