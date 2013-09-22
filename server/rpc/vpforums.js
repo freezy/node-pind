edd7823d4c0519200b75dc91139b8f395ad4a27a
freezy <freezy@xbmc.org>
2013-09-22 13:37:33 +0200

    Confirmed VPF IPDB matches for VP media packs.

--- server/rpc/vpforums.js
+++ server/rpc/vpforums.js
@@ -116,7 +116,7 @@ exports.actions = function(req, res, ss) {
 
 		ipdbmatch: function(params) {
 
-			var category = 36;
+			var category = 35;
 			var p = { where: { category: category }, order: 'title' };
 			var map = vpf.getIpdbMap();
 			var queryStart = +new Date();
