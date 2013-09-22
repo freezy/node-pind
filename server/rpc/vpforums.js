dafa5f3c4686f83d57dda4660472063c845324bd
freezy <freezy@xbmc.org>
2013-09-22 17:53:02 +0200

    Confirmed VPF IPDB matches for VP translite videos.

--- server/rpc/vpforums.js
+++ server/rpc/vpforums.js
@@ -116,7 +116,7 @@ exports.actions = function(req, res, ss) {
 
 		ipdbmatch: function(params) {
 
-			var category = 43;
+			var category = 44;
 			var p = { where: { category: category }, order: 'title' };
 			var map = vpf.getIpdbMap();
 			var queryStart = +new Date();
